6dcaa52394a44bdda317457b01188b4ddaca5e66
TheaSang <ys3421@nyu.edu>
2019-04-23 15:53:01 +0800

    my sideproject

--- src/App.js
+++ src/App.js
@@ -3,23 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = [
+      {color1:``},
+      {color2:``},
+      {showing: false}
+    
+    ]
+  }
+  
+  color1Change = (event) => {
+    let color1Value = event.target.value;
+    this.setState({color1:color1Value})
+  }
+
+
+
+  color2Change = (event) => {
+    let color2Value = event.target.value;
+    this.setState({color2:color2Value})
+  }
+  
+
+
+
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className='title'>
+        Color Mixer
+        </div>
+<div className='box'>
+        <div className='color1'  style={{backgroundColor: this.state.color1}}>
+        Hex Color1:<input onInput = {this.color1Change}></input>
+      </div>
+
+      <div className='color2' style={{backgroundColor: this.state.color2}}>
+        Hex Color2:<input onInput = {this.color2Change}></input>
+      </div>
+
+      </div>
+      
+
+
       </div>
     );
   }
